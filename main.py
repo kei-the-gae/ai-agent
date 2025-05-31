52b890f743b2b7fe80b2bbae842c2b1480218c14
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2025-05-30 23:56:04 -0700

    refactor prompt input to be part of a list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 
@@ -19,15 +20,24 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages)
+
+
+def generate_content(client, messages):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=user_prompt,
+        contents=messages,
     )
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
     print("Response:")
     print(response.text)
 
 
 if __name__ == "__main__":
     main()
+
